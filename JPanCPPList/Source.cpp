c27bf4d0f1dc89a699a0011b75dc3c3bcc6948b4
Johnny Pan <okthen316@gmail.com>
2018-09-23 08:23:26 -0700

    Previous commit failed

--- JPanCPPList/Source.cpp
+++ JPanCPPList/Source.cpp
@@ -9,10 +9,12 @@ int main()
 	DLList list;
 	list.AddToEnd(1);
 	list.AddToEnd(2);
+	list.AddToEnd(3);
 	/*bool running = true;
 	while (running)
 	{
 		cout << "1. Add to front" << endl;
 	}*/
+	system("PAUSE");
 	return 0;
 }
